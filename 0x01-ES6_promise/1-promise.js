d9c62d006cfb7b0ac258df8ccc4a07e276eca5dd
FrancoBuenoHolberton <3290@holbertonschool.com>
2022-04-13 23:00:10 +0000

    fix-task1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,11 +1,11 @@
 export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success == 1) {
+    if (success) {
       resolve({
-	status: 200,
-	body: 'Success',
+        status: 200,
+        body: 'Success',
       });
     }
     reject(new Error('The fake API is not working currently'));
-   }); 
+  });
 }
