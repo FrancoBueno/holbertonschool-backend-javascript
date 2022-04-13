b4f22b95d3b53ce50f1ab8a31138c5dba4e0bdd9
FrancoBuenoHolberton <3290@holbertonschool.com>
2022-04-13 23:47:49 +0000

    fin-2

---
+++ 0x01-ES6_promise/2-then.js
@@ -0,0 +1,9 @@
+export default function handleResponseFromAPI(promise) {
+  const msg = { status: 200, body: 'success' };
+  return promise
+    .then(() => msg)
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+  }
