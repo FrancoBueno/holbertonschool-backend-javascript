16517862c2e47398277475a53a141a9e0a25df37
FrancoBuenoHolberton <3290@holbertonschool.com>
2022-04-19 00:52:54 +0000

    task-8-testing

---
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -0,0 +1,9 @@
+export default function cleanSet(seter, startString) {
+  if (!startString || typeof startString !== 'string' || typeof seter !== 'object') {
+    return '';
+  }
+  return [...seter]
+    .filter((element) => element.startsWith(startString))
+    .map((element) => element.slice(startString.length))
+    .join('-');
+}
